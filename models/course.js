b6da0d64a682910301f9719b421ed808297b89c1
saaketc <saaket123@outlook.comcls>
2020-04-07 00:52:10 +0530

    structure of activity content changed

--- models/course.js
+++ models/course.js
@@ -9,7 +9,16 @@ const courseSchema = mongoose.Schema({
     modules: [
         {
             title: { type: String, required: true, maxlength: 100 },
-            activityContent: [{ title: String, content: String, materials:String, characters: String }],
+            activityContent: [
+            { title: String,
+
+            content:{
+                instructions: String,
+                story: String,
+                parts:[{title: String, content: String}]
+            }, 
+            materials:String, 
+            characters: String }],
             description: { type: String, required: true },
             purpose: { type: String, required: true },
             evaluation: { observational: [String], questions: [String] },
